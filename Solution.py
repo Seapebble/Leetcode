1483f3e4b836a4d346621309d20179b7f485f2c8
Seapebble <seabottlepebble@proton.me>
2025-01-08 04:45:54 +0000

    python

--- Solution.py
+++ Solution.py
@@ -1,13 +1,21 @@
 class Solution:
-    def titleToNumber(self, columnTitle: str) -> int:
-        result = 0
-        for a in columnTitle:
-            value = ord(a) - 64
-            result = result * 26 + value
-        return result
+    def isPalindrome(self, s: str) -> bool:
+        preprocess = []
+        for letter in range(len(s)):
+            if s[letter].isalnum():
+                preprocess.append(s[letter].lower())
+        s=("".join(preprocess))
+        front = 0
+        back = len(s) -1
+        while front < back:
+            if(s[front] != s[back]):
+                return False
+            front += 1
+            back -= 1
+        return True
 
 # Test cases
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.titleToNumber("AAC")) 
-    print(solution.titleToNumber("A"))  
+    print(solution.isPalindrome("race a car")) 
+    print(solution.isPalindrome("A man, a plan, a canal: Panama"))  
